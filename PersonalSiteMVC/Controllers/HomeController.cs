9065a2f1efda15b0a606589edaa756c4d8a97395
Mike Tullis <miketullis@hotmail.com>
2021-10-28 01:33:37 -0500

    Reference Page Added

--- PersonalSiteMVC/Controllers/HomeController.cs
+++ PersonalSiteMVC/Controllers/HomeController.cs
@@ -13,16 +13,9 @@ namespace PersonalSiteMVC.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
-
-        public ActionResult Contact()
+        public ActionResult References()
         {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
